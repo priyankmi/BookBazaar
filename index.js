98c13e2ac242f2c9fe76d8e3eb67d0ecf252e988
priyankmi <127470468+priyankmi@users.noreply.github.com>
2023-08-17 23:38:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer=require("multer");
 const path=require("path");
@@ -13,8 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
